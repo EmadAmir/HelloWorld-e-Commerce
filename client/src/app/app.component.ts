ec08cc77eb177cad7d44253d81393dc59575f1c6
Emad Amir <24242.imad@gmail.com>
2024-10-31 02:31:07 +0000

    Added products to the home page and configured brands and types

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,27 +5,15 @@ import { HttpClient } from '@angular/common/http';
 import { NgFor } from '@angular/common';
 import { ShopService } from './core/services/shop.service';
 import { Product } from './shared/models/product';
+import { ShopComponent } from './features/shop/shop.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NgFor],
+  imports: [RouterOutlet, HeaderComponent, NgFor, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/';
-  private http = inject(HttpClient);
-  private shopServices = inject(ShopService);
-  title = 'client';
-
-  products: Product[] = [];
-
-  ngOnInit(): void {
-    this.shopServices.getProducts().subscribe({
-      next: (repsonse) => (this.products = repsonse.data),
-      error: (error) => console.log(error),
-      complete: () => console.log('Request Completed!!'),
-    });
-  }
+export class AppComponent {
+  title = 'HelloWorld';
 }
