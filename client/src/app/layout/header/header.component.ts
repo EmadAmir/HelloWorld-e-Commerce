bf581eea8466cbc700ff488a8cea5e1671eefc74
Emad Amir <24242.imad@gmail.com>
2024-11-11 22:47:23 +0000

    updated the router with components and added routerlink to to home and shop page

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatButtonModule, MatBadge],
+  imports: [MatIcon, MatButtonModule, MatBadge, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
